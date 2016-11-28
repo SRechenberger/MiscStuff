c4f6c591ec9c3dbb411f237dcc595c7b296fddf0
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2016-11-29 00:18:15 +0100

    added stuff

--- Haskell/dpll/src/Solver.hs
+++ Haskell/dpll/src/Solver.hs
@@ -9,45 +9,46 @@ import Control.Monad.Random
 
 import Data.Set (Set)
 import qualified Data.Set as S
+import Data.List
+import Data.Function
 
-type Solver g a = StateT Output (Rand g) a
+import Data.Text (pack)
+
+type Solver g a = StateT (Int, Output) (Rand g) a
 
 runSolver :: RandomGen g => g -> Solver g a -> Output
-runSolver gen solver = evalRand (execStateT solver (Output [] UNKNOWN S.empty)) gen
+runSolver gen solver = snd $
+  evalRand (execStateT solver (0,(Output [] UNKNOWN S.empty))) gen
 
 solve :: RandomGen g => g -> CNF -> Output
 solve gen cnf = runSolver gen
-    (solve' (S.fromList [1..(cnf ^. numVariables)])
-    (cnf ^. clauses))
+    (do solve' (S.fromList [1..(cnf ^. numVariables)]) (cnf ^. clauses)
+        bts <- use _1
+        comment $ "Backtracks " ++ show bts)
+
+comment :: RandomGen g => String -> Solver g ()
+comment c = _2 . comments %= (++ [pack c])
 
 solve' :: RandomGen g => Set Int -> [Clause] -> Solver g Bool
 solve' vars clauses
-  | null clauses = solution .= SATISFIABLE >> return True
-  | S.empty `elem` clauses = solution .= UNSATISFIABLE >> return False
+  | null clauses = _2 . solution .= SATISFIABLE >> return True
+  | S.empty `elem` clauses = _2 . solution .= UNSATISFIABLE >> return False
   | otherwise = do
-      let units = S.filter (\v -> S.singleton v `elem` clauses) vars
-      if not $ S.null units
-         then do
-           forM_ units $ \u -> values %= S.insert u
-           solve' (vars S.\\ units) $
-            foldl
-              (\cs u -> if u < 0 then set (-u) False cs else set u True cs)
-              clauses units
-         else do
-          (v,x) <- lift $ do
-            v <- uniform $ S.toList vars
-            x <- uniform [True, False]
-            return (v,x)
-
-          let lit = if x then v else -v
-          sat <- solve' (S.delete v vars) (set v x clauses)
-          if sat
-            then values %= S.insert lit >> return True
-            else do
-              sat' <- solve' (S.delete v vars) (set v (not x) clauses)
-              if sat'
-                then values %= S.insert (-lit) >> return True
-                else return False
+      (v,x) <- lift $ do
+        let clauses' = sortBy (compare `on` S.size) clauses
+        v <- uniform $ S.toList (head clauses')
+        x <- uniform [True, False]
+        return (v,x)
+      let lit = if x then v else -v
+      sat <- solve' (S.delete v vars) (set v x clauses)
+      if sat
+        then _2 . values %= S.insert lit >> return True
+        else do
+          _1 += 1
+          sat' <- solve' (S.delete v vars) (set v (not x) clauses)
+          if sat'
+            then _2 . values %= S.insert (-lit) >> return True
+            else return False
 
   where
     set :: Int -> Bool -> [Clause] -> [Clause]
