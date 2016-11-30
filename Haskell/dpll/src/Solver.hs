2bfa1ef596b25ccc5a6809d516c7358466f5191e
Sascha Rechenberger LinuxPool <sascha.rechenberger@uni-ulm.de>
2016-11-30 15:56:26 +0100

    improved dpll solver

--- Haskell/dpll/src/Solver.hs
+++ Haskell/dpll/src/Solver.hs
@@ -16,6 +16,8 @@ import Data.Text (pack)
 
 type Solver g a = StateT (Int, Output) (Rand g) a
 
+for = flip map
+
 runSolver :: RandomGen g => g -> Solver g a -> Output
 runSolver gen solver = snd $
   evalRand (execStateT solver (0,(Output [] UNKNOWN S.empty))) gen
@@ -33,25 +35,71 @@ solve' :: RandomGen g => Set Int -> [Clause] -> Solver g Bool
 solve' vars clauses
   | null clauses = _2 . solution .= SATISFIABLE >> return True
   | S.empty `elem` clauses = _2 . solution .= UNSATISFIABLE >> return False
+  | not (null units) = do
+      let units' = for units $ \u ->
+                    if u < 0 
+                    then (-u, False) 
+                    else (u, True)
+          clauses' = foldr (uncurry set) clauses units'
+      forM_ units $ \l -> _2 . values %= S.insert l
+      solve' (vars S.\\ S.fromList (map abs units)) clauses'
+  | not (null pures) = do
+      vs <- forM pures $ \p -> do
+        _2 . values %= S.insert p
+      
+        let v = if p < 0 then False else True
+        return (abs p, v)
+
+      let vars' = vars S.\\ S.fromList (map abs pures)
+      let clauses' = foldr (uncurry set) clauses vs
+      solve' vars' clauses'
   | otherwise = do
-      (v,x) <- lift $ do
-        let clauses' = sortBy (compare `on` S.size) clauses
-        v <- uniform $ S.toList (head clauses')
-        x <- uniform [True, False]
-        return (v,x)
+      (v,x) <- branch'
       let lit = if x then v else -v
+      s <- use $ _2 . values
       sat <- solve' (S.delete v vars) (set v x clauses)
       if sat
-        then _2 . values %= S.insert lit >> return True
+      then _2 . values %= S.insert lit >> return True
+      else do
+        _2 . values .= s
+        _1 += 1
+        sat' <- solve' (S.delete v vars) (set v (not x) clauses)
+        if sat'
+        then _2 . values %= S.insert (-lit) >> return True
         else do
-          _1 += 1
-          sat' <- solve' (S.delete v vars) (set v (not x) clauses)
-          if sat'
-            then _2 . values %= S.insert (-lit) >> return True
-            else return False
+          _2 . values .= s
+          return False
+
 
   where
     set :: Int -> Bool -> [Clause] -> [Clause]
     set var True = filter (var `S.notMember`) . map (S.filter (/= (-var)))
     set var False = set (-var) True
 
+    units :: [Int]
+    units = concat . map S.toList . filter ((==1) . S.size) $ clauses
+
+    vars' :: Set Int
+    vars' = S.unions clauses
+
+    pures :: [Int]
+    pures =
+      [i | i <- S.toList vars', S.member i vars' && S.notMember (-i) vars']
+    
+    branch :: RandomGen g => Solver g (Int, Bool)
+    branch = lift $ do
+      let clauses' = sortBy (compare `on` S.size) clauses
+      v <- uniform $ S.toList (head clauses')
+      return (v,if v < 0 then False else True)
+    
+    branch' :: RandomGen g => Solver g (Int, Bool)
+    branch' = lift $ do
+      let clause = head $ reverse $ sortBy (compare `on` S.size) clauses
+      v1 <- uniform $ S.toList clause
+      v2 <- uniform $ S.toList clause
+      v3 <- uniform $ S.toList clause
+      let v = maximumBy (compare `on` cnt) [v1,v2,v3]
+      return (abs v, if v < 0 then False else True)
+      
+
+    cnt i = sum . map fromEnum . map (\s -> S.member i s) $ clauses
